a65b3eb7e984d19c74ec92147637af7fca60ffe8
kakurokan <leokakali7@gmail.com>
2025-11-06 13:44:24 +0000

    Leitura do input

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+from sympy import *
+
+init_printing(use_unicode=True)
+
+
 def main():
-    print("Hello World!")
+    n = int(input("Insira o número de equações/incógnitas: "))
+    print(
+        "Escreva a matriz A (coeficientes) de tamanho n x n, separando as colunas por espaço e linhas por parágrafos:\n")
+
+    matriz = []
+    valores_b = []
+
+    for i in range(n):
+        numeros = input().split(" ")
+        numeros_sympy = [sympify(elemento) for elemento in numeros]
+        matriz.append(numeros_sympy)
+
+    linha = input("Escreva os valores da matriz coluna B de tamanho n, separados por espaço")
+    partes = linha.split(" ")
+    for i in partes:
+        valores_b.append(sympify(i))
+
+    tol = input("Insira a tolerância absoluta: ")
+    n_max = input("Insira o número máximo de iterações: ")
+
 
 main()
